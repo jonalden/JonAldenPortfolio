ac1977d28672dc041a8f3327805b38d3f6139055
jon alden <jondevalden@gmail.com>
2019-09-19 15:48:49 -0500

    astronaut image scrolls left to right, looking into scrolling from top-left to bottom-right on scroll.

--- assets/JavaScript/imageScroll.js
+++ assets/JavaScript/imageScroll.js
@@ -1,3 +1,12 @@
-$('#ParentContainer').scroll(function() { 
-    $('#FixedDiv').animate({top:$(this).scrollTop()},100,"linear");
-})
+let current = $(window).scrollTop();
+const total = $(window).height() - current;
+const ele = $(".astronautImage");
+const currPosition = ele.position().left;
+const trackLength = 250;
+$(window).scroll(function (event) {
+    current = $(window).scrollTop();
+    console.log({ total: total, current: current });
+    console.log(current / total * 100);
+    const newPosition = trackLength * (current / total)
+    ele.css({ left: currPosition + newPosition * (8) + 'px' });
+});
